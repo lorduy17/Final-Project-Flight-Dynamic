e213b25ccb7c0b2bfac916de1223d0facb717e54
lorduy17 <pablolorduy2004@gmail.com>
2025-08-31 21:05:09 -0500

    Update main.py

--- FD/main.py
+++ FD/main.py
@@ -1,14 +1,16 @@
 import tkinter as tk
 from objeto import maya
 from interfaz import Interfaz
+import os
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.title("Rotación STL con sliders")
 
-    obj = maya()
-    app = Interfaz(root, obj)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    root = tk.Tk()
+    root.title("3D STL Viewer")
+    app = Interfaz(root)
 
     root.mainloop()
+
 
     
