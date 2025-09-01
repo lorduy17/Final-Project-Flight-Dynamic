6f2920cdb6856c930c6529f16c21a41299773594
lorduy17 <pablolorduy2004@gmail.com>
2025-08-31 21:06:28 -0500

    Update objeto.py

--- FD/objeto.py
+++ FD/objeto.py
@@ -6,18 +6,32 @@ from tkinter import filedialog
 class maya():
 
     def __init__(self):
-        self.ruta_stl = None
+        self.model = None
+        self.original_vectors = None
+        self.normalized_vectors = None
 
-    def carga_archivo(self):
-        self.ruta_stl = filedialog.askopenfilename(title="Selecciona archivo STK",filetypes=[("Archivos STL","*stl")])
-        if not self.ruta_stl:
-            raise ValueError("No se seleccionó archivo STL")
-       
-    def solido(self):
-        if not self.ruta_stl:
-            raise ValueError("No hay archivo STL cargado")
-        
-        modelo = mesh.Mesh.from_file(self.ruta_stl)
-        return modelo.vectors
+    def file_load(self, filename: str):
+            if not filename:
+                raise ValueError("STL file not found")
+            self.model = mesh.Mesh.from_file(filename)
+            self.original_vectors = self.model.vectors.copy()
+            self.normalized_vectors = None
+
+    def get_original_vertices(self):
+         if self.original_vectors is None:
+             raise ValueError("No STL file loaded")
+         return self.original_vectors
     
+    def get_normalized_vertices(self):
+        if self.original_vectors is None:
+            raise ValueError("No STL file loaded")
+        
+        if self.normalized_vectors is not None:
+            return self.normalized_vectors
+        
+        model_flat = self.model.vectors.reshape(-1,3)
 
+        max = np.max(model_flat)
+        normalized = model_flat/ max
+        self.normalized_vectors = normalized.reshape(self.model.vectors.shape)
+        return self.normalized_vectors
